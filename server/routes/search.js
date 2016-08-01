f3ba426001c2485679a640c5828e402d5c226eda
Markus Stenqvist <daysnine@gmail.com>
2016-08-01 13:40:28 +0200

    Removed async lib and replaced it with simpler native promises
    Promises > Async. :)

--- server/routes/search.js
+++ server/routes/search.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const async = require("async");
 const moment = require('moment');
 
 router.post('/', function(req, res, next) {
@@ -12,7 +11,7 @@ router.post('/', function(req, res, next) {
   const airport_url = 'http://node.locomote.com/code-task/airports?q=';
   const dateIsTooEarly = moment().isAfter(moment(date, 'YYYY-MM-DD'), 'day') || moment().isSame(moment(date, 'YYYY-MM-DD'), 'day');
   if (!destination || !origin || !date || dateIsTooEarly) {
-    return res.json({
+    res.json({
       error: "Fields are incorrect or date is in the past.",
       date: date,
       destination: destination,
@@ -23,30 +22,42 @@ router.post('/', function(req, res, next) {
     });
   }
 
-  async.waterfall([
-    function(callback) {
-      request('http://node.locomote.com/code-task/airlines', (error, response, body) => {
-        callback(error, JSON.parse(body));
-      });
-    },
-    function(airlines, callback) {
-      request(airport_url + destination, (error, response, body) => {
-        callback(error, airlines, JSON.parse(body));
-      });
-    },
-    function(airlines, possibleDestinations, callback) {
-      request(airport_url + origin, (error, response, body) => {
-        callback(error, airlines, possibleDestinations, JSON.parse(body));
-      });
-    },
-    function(airlines, possibleDestinations, possibleOrigins, callback) {
-      // Get first airport or origin and destination
-      // TODO: Let the user choose if there is several options
-      const destinationAirportCode = possibleDestinations.length > 0 ? possibleDestinations[0].airportCode : null;
-      const originAirportCode = possibleOrigins.length > 0 ? possibleOrigins[0].airportCode : null;
-      callback(null, airlines, destinationAirportCode, originAirportCode);
-    }
-  ], function (err, airlines, destinationAirportCode, originAirportCode) {
+  const airlinesPromise = new Promise(function(resolve, reject) {
+    request('http://node.locomote.com/code-task/airlines', (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+
+  const possibleDestinationsPromise = new Promise(function(resolve, reject) {
+    request(airport_url + destination, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+
+  const possibleOriginsPromise = new Promise(function(resolve, reject) {
+    request(airport_url + origin, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+
+  Promise.all([airlinesPromise, possibleDestinationsPromise, possibleOriginsPromise]).then(values => {
+    // Using node stable, can't use destructing yet :(
+    const airlines = values[0];
+    const possibleDestinations = values[1];
+    const possibleOrigins = values[2];
+    let responseJSON = null;
+
+    const destinationAirportCode = possibleDestinations.length > 0 ? possibleDestinations[0].airportCode : null;
+    const originAirportCode = possibleOrigins.length > 0 ? possibleOrigins[0].airportCode : null;
     if (destinationAirportCode === null || originAirportCode === null) {
       res.json({
         error: "There was an error with the destination or origin. Please send a valid city/airport.",
@@ -56,39 +67,47 @@ router.post('/', function(req, res, next) {
         possibleDestinations: possibleDestinations
       });
     } else {
-      async.times(airlines.length, function(n, callback) {
-        const flight_search_url = 'http://node.locomote.com/code-task/flight_search/' + airlines[n].code + '?date=' + date + '&from=' + originAirportCode + '&to=' + destinationAirportCode;
-        request(flight_search_url, function(error, response, body) {
-            if (error) {
-              callback(error);
-            }
-            if (res.statusCode !== 200) {
-              callback(res.statusCode);
-            }
-            callback(error, JSON.parse(body));
-        });
 
-      }, function(error, flights) {
-        if (error) {
-          res.json({
-            error: "There was an error while calculating flights",
-            destinationAirportCode: destinationAirportCode,
-            originAirportCode: originAirportCode,
-            possibleOrigins: possibleOrigins,
-            possibleDestinations: possibleDestinations,
-            flights: flights
+      const searchPromises = airlines.map(airline => {
+        return new Promise(function(resolve, reject) {
+          const flight_search_url = 'http://node.locomote.com/code-task/flight_search/' + airline.code + '?date=' + date + '&from=' + originAirportCode + '&to=' + destinationAirportCode;
+          request(flight_search_url, function(error, response, body) {
+              if (error) {
+                reject(error);
+              }
+              if (res.statusCode !== 200) {
+                reject(res.statusCode);
+              }
+              resolve(JSON.parse(body));
           });
-        } else {
-          let singleFlightsArray = [];
-          flights.forEach(flight_array => {
-            flight_array.forEach(flight => {
-              singleFlightsArray.push(flight);
-            });
+        });
+      });
+
+      Promise.all(searchPromises).then(flights => {
+        let singleFlightsArray = [];
+        flights.forEach(flight_array => {
+          flight_array.forEach(flight => {
+            singleFlightsArray.push(flight);
           });
-          res.json(singleFlightsArray);
-        }
+        });
+        res.json(singleFlightsArray);
+      }, reason => {
+        res.json({
+          error: "There was an error while calculating flights",
+          trace: reason,
+          destinationAirportCode: destinationAirportCode,
+          originAirportCode: originAirportCode,
+          possibleOrigins: possibleOrigins,
+          possibleDestinations: possibleDestinations,
+          flights: flights
+        });
       });
     }
+  }, reason => {
+    res.json({
+      error: "There was an error while doing a request",
+      trace: reason
+    });
   });
 });
 
